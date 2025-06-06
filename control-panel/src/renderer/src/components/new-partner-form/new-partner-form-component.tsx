f245edcfbedd991b97c5a650e6b868d5851c6bad
Joseph Dvorak <dvorak@Josephs-iMac.local>
2025-06-06 00:29:37 -0700

    added textarea component

--- control-panel/src/renderer/src/components/new-partner-form/new-partner-form-component.tsx
+++ control-panel/src/renderer/src/components/new-partner-form/new-partner-form-component.tsx
@@ -1,9 +1,12 @@
 import { useContext } from 'react';
+import { useForm, useValue, usePipe } from 'fully-formed';
 import { PartnersContext } from '@renderer/contexts/partners-context/partners-context';
-import { useForm, useValue } from 'fully-formed';
 import { NewPartnerForm } from './new-partner-form';
 import { InputGroup } from '../input-group';
 import { ImageInput } from '../image-input';
+import { Label } from '../label';
+import { TextArea } from '../textarea/textarea';
+import { Messages } from '../messages';
 import styles from './styles.module.scss';
 
 export function NewPartnerFormComponent() {
@@ -11,6 +14,9 @@ export function NewPartnerFormComponent() {
   const form = useForm(new NewPartnerForm(partners));
   const partnerName = useValue(form.fields.name);
   const logoAltText = `${partnerName || 'Partner'} Logo`;
+  const hideDescriptionMessages = usePipe(form.fields.description, (state) => {
+    return !(state.hasBeenModified || state.hasBeenBlurred || state.submitted);
+  });
 
   return (
     <form className={styles.form}>
@@ -34,6 +40,16 @@ export function NewPartnerFormComponent() {
         name="logo"
         label="Logo*"
         thumbnailAlt={logoAltText}
+        containerClassName={styles.logo_input}
+      />
+      <Label field={form.fields.description} variant="floating">
+        Description*
+      </Label>
+      <TextArea field={form.fields.description} />
+      <Messages
+        messageBearers={[form.fields.description]}
+        hideMessages={hideDescriptionMessages}
+        containerClassName={styles.messages}
       />
       <InputGroup
         type="text"
--- control-panel/src/renderer/src/components/textarea/textarea.tsx
+++ control-panel/src/renderer/src/components/textarea/textarea.tsx
@@ -1,92 +1,71 @@
-import { useId, type CSSProperties, type ChangeEventHandler } from 'react';
+import { useRef, useId, useEffect, type CSSProperties } from 'react';
 import {
   useFocusEvents,
   useMultiPipe,
+  useUserInput,
   ValidityUtils,
   type FieldOfType,
   type IGroup,
   type Field,
-  type Group,
-  useValue
+  type Group
 } from 'fully-formed';
 import styles from './styles.module.scss';
 
-interface InputProps {
+interface TextAreaProps {
   /**
-   * A {@link Field} that will control the state of the input.
+   * A {@link Field} that will control the state of the textarea.
    */
   field: FieldOfType<string>;
   /**
    * An array of {@link Group}s. If these groups' validators have
-   * executed and returned an invalid result, the input will appear invalid.
+   * executed and returned an invalid result, the textarea will appear invalid.
    */
   groups?: IGroup[];
-  type: string;
   /**
    * If true, the placeholder and value text will be opaque even before the user
    * has interacted with the field. Set this to true if you are pairing the
-   * input with a stationary label. Set this to false if you are pairing the
-   * input with a floating label.
+   * textarea with a stationary label. Set this to false if you are pairing the
+   * textarea with a floating label.
    */
   showText?: boolean;
   placeholder?: string;
   disabled?: boolean;
-  autoComplete?: string;
   maxLength?: number;
-  max?: string;
   className?: string;
   style?: CSSProperties;
   ['aria-required']?: boolean;
   ['aria-describedby']?: string;
-  allowedCharacters?: RegExp | string[];
 }
 
 /**
- * Renders an HTML input element whose value and appearance are controlled by
+ * Renders an HTML textarea element whose value and appearance are controlled by
  * the state of a {@link Field}.
  *
- * @param props - {@link InputProps}
+ * @param props - {@link TextAreaProps}
  *
  * @remarks
- * If the provide field is invalid, the returned input element will appear
+ * If the provide field is invalid, the returned textarea element will appear
  * invalid once the user has interacted with it.
  *
- * If `groups` are provided, the field will appear invalid if any of those
+ * If `groups` are provided, the textarea will appear invalid if any of those
  * groups' validators have failed and the user has interacted with it.
  */
-export function Input({
+export function TextArea({
   field,
   groups = [],
-  type,
   showText,
   placeholder,
   disabled,
-  autoComplete,
   maxLength,
-  max,
   className: classNameProp,
   style,
   ['aria-required']: ariaRequired,
-  ['aria-describedby']: ariaDescribedBy,
-  allowedCharacters
-}: InputProps) {
-  const value = useValue(field);
-  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    if (allowedCharacters) {
-      const characters = e.target.value.split('');
-      if (allowedCharacters instanceof RegExp) {
-        if (characters.some((c) => !allowedCharacters.test(c))) {
-          return;
-        }
-      } else if (characters.some((c) => !allowedCharacters.includes(c))) {
-        return;
-      }
-    }
-
-    field.setValue(e.target.value);
-  };
+  ['aria-describedby']: ariaDescribedBy
+}: TextAreaProps) {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const { value, onChange } = useUserInput(field);
   const className = useMultiPipe([field, ...groups], (states) => {
-    const classNames = [styles.input];
+    const classNames = [styles.textarea];
     const fieldState = states[0];
 
     if (
@@ -147,17 +126,25 @@ export function Input({
 
   const warningMessageId = useId();
 
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = '43px';
+      const newHeight = Math.max(textAreaRef.current.scrollHeight, 43);
+      textAreaRef.current.style.height = newHeight + 'px';
+    }
+  }, [value]);
+
   return (
     <>
-      <input
+      <textarea
         name={field.name}
         id={field.id}
-        type={type}
+        ref={textAreaRef}
+        value={value}
+        onChange={onChange}
         placeholder={placeholder}
         disabled={disabled}
-        autoComplete={autoComplete}
         maxLength={maxLength}
-        max={max}
         aria-required={ariaRequired}
         aria-describedby={
           ariaDescribedBy
@@ -165,8 +152,6 @@ export function Input({
             : warningMessageId
         }
         aria-invalid={ariaInvalid}
-        value={value}
-        onChange={onChange}
         {...useFocusEvents(field)}
         className={className}
         style={style}
